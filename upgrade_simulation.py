30a4c1782f051dd5000c48f6905cfa5432f9dbca
wcrib8 <wolfcribbs8@gmail.com>
2026-05-18 16:15:05 -0600

    first steps with initial python file

---
+++ upgrade_simulation.py
@@ -0,0 +1,8 @@
+# go through dictionary of save data for each player
+# list each of that player's stats for each round
+
+def display_stats(stats_dic):
+    for player in stats_dic:
+        print(player, " ")
+    print('\n')
+    # for num of rounds, print each player's stats?
