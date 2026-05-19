4ab0c95ed9e54049941d10bedde4f0704bb214a0
wcrib8 <wolfcribbs8@gmail.com>
2026-05-18 19:51:18 -0600

    changed from outputting to terminal to displaying a graph with matplotlib

--- upgrade_simulation.py
+++ upgrade_simulation.py
@@ -1,42 +1,58 @@
 import json
 from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
 
 folder = Path("SaveData")
+stats_keys = ['reload', 'resistance', 'speed', 'vision']
 
-# load data
-def display_stats():
-    all_stats = []
-    # go through each file in folder, then print stats for each round for that player
-    for file in folder.glob('*.jsonl'):
-        name = file.stem
-        #name = []
-        print(name, ":\n")
-        #file.player_name
-        curr_round = 0
-        with open(file, 'r') as f:
-            for line in f:
-                entry = json.loads(line)
-                #name.append(json.loads(line))
+# collect data
+# go through each file in folder, then print stats for each round for that player
+players = {}
+for file in folder.glob('*.jsonl'):
+    if file.stem == 'game_params':
+        continue
+    name = file.stem
+    players[name] = {}
+    #print(name, ":\n")
+    #curr_round = 0
+    with open(file, 'r') as f:
+        for line in f:
+            entry = json.loads(line)
+            round_num = entry['round']
+            players[name][round_num] = entry['upgrade_stats']
 
-                # if the round changes, print the stats for that round
-                if entry['round'] != curr_round:
-                    curr_round += 1
-                    print("Round ", curr_round, ":")
-                    for stat in entry['upgrade_stats']:
-                        print(stat, ":", entry['upgrade_stats'][stat], " ")
-                    print()
+            #if the round changes, print the stats for that round (console print version)
+            # if entry['round'] != curr_round:
+            #     curr_round += 1
+            #     print("Round ", curr_round, ":")
+            #     for stat in entry['upgrade_stats']:
+            #         print(stat, ":", entry['upgrade_stats'][stat], " ")
+            #     print()
 
-        #all_stats.append(name)
-    return
-    
+#plot
+player_names = list(players.keys())
+n_players = len(player_names)
+all_rounds = sorted(set(r for p in players.values() for r in p.keys()))
+x = np.arange(len(all_rounds))
+width = 0.8 / len(stats_keys)
 
-# go through dictionary of save data for each player
-# list each of that player's stats for each round
+fig, axes = plt.subplots(1, len(player_names), figsize=(12, 5), sharey=True)
+fig.suptitle('Player Upgrade Stats by Round')
 
-# def display_stats(stats_dic):
-#     for player in stats_dic:
-#         print(player, " ")
-#     print('\n')
-#     # for num of rounds, print each player's stats?
+for ax, name in zip(axes, player_names):
+    for i, stat in enumerate(stats_keys):
+        values = [players[name].get(r, {}).get(stat, 0) for r in all_rounds]
+        offset = (i - len(stats_keys) / 2) * width + width / 2
+        ax.bar(x + offset, values, width=width, label=stat)
+    ax.set_title(name)
+    ax.set_xlabel('Round')
+    ax.set_ylabel('Value')
+    ax.set_xticks(x)
+    ax.set_xticklabels([f'Round {r}' for r in all_rounds])
+    ax.legend()
 
-display_stats()
+plt.tight_layout()
+plt.show()
+
+#display_stats()
